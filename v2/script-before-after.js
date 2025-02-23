508203b6ae81d0f4dbe252a1b7f364a042532822
Илья Евсеенко <addminuser258@gmail.com>
2025-02-24 00:42:59 +0300

    много услуг добавил

--- v2/script-before-after.js
+++ v2/script-before-after.js
@@ -3,5 +3,14 @@
 
 function beforeAfter() {
   document.getElementById("before_after1").style.width =
-document.getElementById("before_after_slider").value + "%";
+  document.getElementById("before_after_slider1").value + "%";
+
+  document.getElementById("before_after2").style.width =
+  document.getElementById("before_after_slider2").value + "%";
+
+  document.getElementById("before_after3").style.width =
+  document.getElementById("before_after_slider3").value + "%";
+
+  document.getElementById("before_after4").style.width =
+  document.getElementById("before_after_slider4").value + "%";
 }
